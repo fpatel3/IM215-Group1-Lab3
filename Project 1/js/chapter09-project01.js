4d23fc8345636a7d182d9fd0b033403a799181d7
fpatel3 <forampatel3010@gmail.com>
2021-02-01 13:01:37 -0500

    made changes

--- Project 1/js/chapter09-project01.js
+++ Project 1/js/chapter09-project01.js
@@ -1,123 +1,36 @@
+window.addEventListener('load', function(){
+    
+ 
+  var form=document.getElementById("mainForm");
+  form.addEventListener("submit", submissionCheck);
 
-/* add code here  */
-
-
-
-function setBackground(e) {
-    if (e.type == "focus") { e.target.classlist.add("highlightable")
-    }
-    else if (e.type == "blur") {
-        e.target.style.classlist.remove("highlightable")//done by vaidehi refrences lab 's note
-    }
-
-    window.addEventListener("load", function () {
-        var cssSelector = "input[type=text],select[name=subject],select[name=genre]";
-        var fields = document.querySelectorAll(cssSelector);
-        for (i = 0; i < fields.length; i++) {
-            fields[i].addEventListener("focus", setBackground);
-            fields[i].addEventListener("blur", setBackground);
-        }   //code done by vaidehi       
-      });
-}
-
-
-
-// window.addEventListener("load",function(){
-// var form = document.getElementById("mainForm");
-// form.addEventListener("submit",submissionCheck);
-// function submissionCheck(event){
-  
-//   var requiredInputs = document.querySelectorAll(".required");
-
-//       for(i=0; i < requiredInputs.length; i++){
-//             if(requiredInputs[i].nodeValue == "")
-//             {
-//                 event.preventDefault();                
-//                 requiredInputs[i].classList.add("error");
-//                 alert("You are trying to submit the form");
-//             }
-
-//             else{
-//                 requiredInputs.classList.remove("error");
-//             }
-//           }
-// }
-// });
-
-
-
-
-      
-       
-//    }
-//    // code done by foram..
-// }
-function empty()
-{
-  var x,y,z;
-  x = document.getElementById("title").value; 
-
-  y =  document.getElementById("description").value;
-  z =  document.getElementById("year").value;
-  if (x == "" || y == "" || z == "") 
-   { 
-       if(x == "")
-       {
-           console.log(x);
-           
-            alert("Enter a Valid title");
-            
-            return false;
-    //   submit.addEventListener(
-    //       "click", function(event){
-    //     event.preventDefault();
-    //         });
-            
-       }
-       else if(y == "")
-       {
-        console.log(y);
-
-      alert("Enter a Valid description");
-    //   submit.addEventListener("click", function(event){
-    //     event.preventDefault();
-     // });
-      return false;
-       }
-      else if(z == "")
-      {
-     alert("Enter a Valid year");
-    //  submit.addEventListener("click", function(event){
-    //     event.preventDefault();
-    //   });
-    return false;
+  function submissionCheck(event){
+    var requiredInputs = document.querySelectorAll(".required");
+    for(i=0; i < requiredInputs.length; i++){
+      if(requiredInputs[i].value == "") {
+        event.preventDefault();
+        requiredInputs[i].classList.add("error");
       }
-      
-       
-   }
-   // code done by foram..
-}
-
-
-
-
-
-function removeClass(selector, myClass) {
-
-    // get all elements that match our selector
-    elements = document.querySelectorAll(selector);
-  
-    // remove class from all chosen elements
-    for (var i=0; i<elements.length; i++) {
-      elements[i].classList.remove(myClass);
+     else {
+        requiredInputs[i].classList.remove("error");
+      }
     }
   }
-  
-  // usage examples:
-  removeClass('.class-selector', 'example-class');
-  removeClass('#id-selector', 'example-class');
-  // end of #4
-  // code done by sharmin
 
-   
+  form.addEventListener("reset", resetForm)
 
+  function resetForm(event){
+    var requiredInputs = document.querySelectorAll(".required");
+    for(i=0; i < requiredInputs.length; i++){
+      requiredInputs[i].classList.remove("error");
+    }
+  }
+
+    var highlightablelnputs = document.querySelectorAll(".hilightable") ; 
+    for(i=0; i < highlightablelnputs.length; i++){ 
+    var toggle = (event) => { event. target. classList. toggle('highlight') }
+    highlightablelnputs[i].addEventListener('focus', toggle) 
+    highlightablelnputs[i].addEventListener('.blur', toggle)
+
+    }
+})
--- project2/project2/js/chapter09-project02.js
+++ project2/project2/js/chapter09-project02.js
@@ -1,1 +1,16 @@
 /* add code here */
+
+
+var img =  document.getElementById("thumbnails");
+
+img.addEventListener("click",changeSize)
+
+
+    // window.addEventListener("load", function () {
+    //     var cssSelector = "input[type=text],select[name=subject],select[name=genre]";
+    //     var fields = document.querySelectorAll(cssSelector);
+    //     for (i = 0; i < fields.length; i++) {
+    //         fields[i].addEventListener("focus", setBackground);
+    //         fields[i].addEventListener("blur", setBackground);
+    //     }   //code done by vaidehi       
+    //   });
