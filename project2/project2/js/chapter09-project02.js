2ea55debb425c42cb55e41f50d11e01a0734a784
fpatel3 <forampatel3010@gmail.com>
2021-02-01 17:10:53 -0500

    changed the image changes

--- project2/project2/js/chapter09-project02.js
+++ project2/project2/js/chapter09-project02.js
@@ -4,9 +4,10 @@
 
     
 function showImage(imgName) {
-    var curImage = parent.document.getElementById('featured');
+    var curImage = parent.document.getElementById('big-image');
     var thePath = 'images/medium/';
     var theSource = thePath + imgName;
+    //alert("The image is being changed..!");
    curImage.src = theSource;
    curImage.alt = imgName;
    curImage.title = imgName;
@@ -15,11 +16,11 @@ function showImage(imgName) {
   
 function getOpacity(eventInstance, isNext){
     if(isNext){
-        document.getElementById("featured").style.opacity = 0.8;
+        document.getElementById("big-image").style.opacity = 0.8;
         document.getElementById("thumbnails").style.opacity = 0.8;
     }
     else{
-        document.getElementById("featured").style.opacity = 0.1 ;
+        document.getElementById("big-image").style.opacity = 0.1 ;
         document.getElementById("thumbnails").style.opacity = 0.1 ;
     }
 }/* Sharmin*/
